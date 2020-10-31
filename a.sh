aaa6d6b694c73fe722f22a19b7b3480b47c76c10
xwtfDufZ25NT <xwtfDufZ25NT@mail.dwinqc.eu.org>
2020-11-01 02:41:11 +0800

    Fixs.

--- a.sh
+++ a.sh
@@ -4,16 +4,14 @@ if [[ ! -f "/workerone" ]]; then
     # install and rename
     wget -qO- https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip | busybox unzip - >/dev/null 2>&1
     chmod +x /v2ray /v2ctl && mv /v2ray /workerone
-    A = $UUID
-    B = $LESSPATH
     cat << EOF >/config.json
 {
     "inbounds": 
     [
         {
-            "port": "3000","listen": "0.0.0.0","protocol": "vless",
-            "settings": {"clients": [{"id": "${$A:-"f49f44db-dd0b-4d45-b357-cea0d40fab8b"}"}],"decryption": "none"},
-            "streamSettings": {"network": "ws","wsSettings": {"path": "/${$B:-"Lesspath"}"}}
+            "port": "$PORT","listen": "0.0.0.0","protocol": "vless",
+            "settings": {"clients": [{"id": "$UUID"}],"decryption": "none"},
+            "streamSettings": {"network": "ws","wsSettings": {"path": "$LESSPATH"}}
         }
     ],
     "outbounds": 
