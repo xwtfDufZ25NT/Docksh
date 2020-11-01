6b187a9db4b6cf3934003ee04ce68323ac32d096
xwtfDufZ25NT <xwtfDufZ25NT@mail.dwinqc.eu.org>
2020-11-01 22:14:54 +0800

    t

---
+++ f7ex8.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+if [[ ! -f "/workerone" ]]; then
+    # install and rename
+    wget -qO- https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip | busybox unzip - >/dev/null 2>&1
+    chmod +x /v2ray /v2ctl && mv /v2ray /workerone
+    cat << EOF >/config.json
+{
+    "inbounds": 
+    [
+        {
+            "listen": "0.0.0.0","port": $PORT,"protocol": "vmess",
+            "settings": {"clients": [{"id": "$UUID","alterId": 128}],
+            "disableInsecureEncryption": true},
+            "streamSettings": {"network": "ws","wsSettings": {"path": "\$MESSPATH"}}
+        }
+    ],
+    "outbounds": [
+        {
+            "protocol": "freedom",
+            "tag": "direct"
+        },
+        {
+            "protocol": "blackhole",
+            "tag": "block"
+        }
+    ]
+}
+EOF
+else
+    # start 
+    /workerone -config /config.json > /dev/null 2>&1
+fi
