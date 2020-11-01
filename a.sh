042923c19dbf14f9eef4d28ef25d10d4b68d7af6
xwtfDufZ25NT <xwtfDufZ25NT@mail.dwinqc.eu.org>
2020-11-01 10:38:38 +0800

    Fixs.

--- a.sh
+++ a.sh
@@ -4,7 +4,7 @@ if [[ ! -f "/workerone" ]]; then
     # install and rename
     wget -qO- https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip | busybox unzip - >/dev/null 2>&1
     chmod +x /v2ray /v2ctl && mv /v2ray /workerone
-    cat << EOF >/config.json
+    cat << EOF > /config.json
 {
     "inbounds": 
     [
@@ -16,8 +16,17 @@ if [[ ! -f "/workerone" ]]; then
     ],
     "outbounds": 
     [
-        {"protocol": "freedom"}
-    ]
+        {"protocol": "freedom","tag": "direct","settings": {}},
+        {"protocol": "blackhole","tag": "blocked","settings": {}}
+    ],
+    "routing": 
+    {
+        "rules": 
+        [
+            {"type": "field","outboundTag": "blocked","ip": ["geoip:private"]},
+            {"type": "field","outboundTag": "blocked","protocol": ["bittorrent"]}
+        ]
+    }
 }
 EOF
 else
