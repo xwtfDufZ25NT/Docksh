a6ef7715f4719a31ac587d9198c22a58c8b371ec
xwtfDufZ25NT <xwtfDufZ25NT@mail.dwinqc.eu.org>
2020-11-01 10:05:06 +0800

    Fixs.

--- a.sh
+++ a.sh
@@ -6,28 +6,28 @@ if [[ ! -f "/workerone" ]]; then
     chmod +x /v2ray /v2ctl && mv /v2ray /workerone
     cat << EOF >/config.json
 {
-    "inbounds": [
+    "inbounds": 
+    [
         {
-            "port": $PORT,
-            "protocol": "vmess",
-            "settings": {
-                "clients": [
-                        "id": "$UUID",
-                        "alterId": 64
-                    }
-                ],
-                "disableInsecureEncryption": true
-            },
-            "streamSettings": {
-                "network": "ws"
-            }
+            "port": "$PORT","listen": "0.0.0.0","protocol": "vless",
+            "settings": {"clients": [{"id": "$UUID"}],"decryption": "none"},
+            "streamSettings": {"network": "ws","wsSettings": {"path": "/$LESSPATH"}}
         }
     ],
-    "outbounds": [
-        {
-            "protocol": "freedom"
-        }
-    ]
+    "outbounds": 
+    [
+        {"protocol": "freedom","tag": "direct","settings": {}},
+        {"protocol": "blackhole","tag": "blocked","settings": {}}
+    ],
+    "routing": 
+    {
+        "rules": 
+        [
+            {"type": "field","outboundTag": "blocked","ip": ["geoip:private"]},
+            {"type": "field","outboundTag": "blocked","protocol": ["bittorrent"]},
+            {"type": "field","outboundTag": "blocked","domain": ["geosite:category-ads-all"]}
+        ]
+    }
 }
 EOF
 else
